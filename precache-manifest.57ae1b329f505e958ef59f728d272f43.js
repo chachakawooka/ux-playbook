b20e1d66905ca24b169bbc85b4dbf9ba0f5ce033
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-30 16:37:50 +0000

    Deploy chachakawooka/ux-playbook to github.com/chachakawooka/ux-playbook.git:gh-pages

---
+++ precache-manifest.57ae1b329f505e958ef59f728d272f43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47afe29b98b352643d0865c912f8529c",
+    "url": "/ux-playbook/index.html"
+  },
+  {
+    "revision": "7e7fd6bbb505c18d5172",
+    "url": "/ux-playbook/static/css/main.4ec7ff3f.chunk.css"
+  },
+  {
+    "revision": "55f72d9ba06240d54ff0",
+    "url": "/ux-playbook/static/js/2.c6461e1d.chunk.js"
+  },
+  {
+    "revision": "7e7fd6bbb505c18d5172",
+    "url": "/ux-playbook/static/js/main.9bccad40.chunk.js"
+  },
+  {
+    "revision": "5b90c484883c984fcf0a",
+    "url": "/ux-playbook/static/js/runtime~main.e22053b7.js"
+  }
+]);
